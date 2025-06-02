d114b369dbe88cd1e2e5a18d2e730d4276b4404b
Ludger Solbach <soulman@soulspace.org>
2025-06-02 21:33:25 +0200

    updated fastmath library

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.soulspace/qclojure "0.1.0"
+(defproject org.soulspace/qclojure "0.1.0-SNAPSHOT"
   :description "A library to implement and simulate quantum and hybrid algorithms in idiomatic Clojure"
   :license {:name "Eclipse Public License 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.clojure/spec.alpha "0.5.238"]
                  [org.clojure/test.check "1.1.1"]
-                 [generateme/fastmath "2.4.0"]
+                 [generateme/fastmath "3.0.0-alpha3"]
                  [hiccup "2.0.0-RC5"]]
 
   :scm {:name "git" :url "https://github.com/lsolbach/QClojure"}

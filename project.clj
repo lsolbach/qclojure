083dcc9009d8d62d14e7d847866a54cd96521cea
Ludger Solbach <soulman@soulspace.org>
2025-06-02 09:22:25 +0200

    updated project file

--- project.clj
+++ project.clj
@@ -1,12 +1,14 @@
 (defproject org.soulspace/qclojure "0.1.0"
-  :description "A library to implement quantum and hybrid algorithms in idiomatic Clojure"
+  :description "A library to implement and simulate quantum and hybrid algorithms in idiomatic Clojure"
+  :license {:name "Eclipse Public License 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.clojure/spec.alpha "0.5.238"]
                  [org.clojure/test.check "1.1.1"]
                  [generateme/fastmath "2.4.0"]
                  [hiccup "2.0.0-RC5"]]
 
-  :scm {:name "git" :url "https://github.com/lsolbach/QClojure"} 
+  :scm {:name "git" :url "https://github.com/lsolbach/QClojure"}
   :deploy-repositories [["clojars" {:sign-releases false :url "https://clojars.org/repo"}]]
   ;
   )

90bac3607840c7d64b9af10b3b81498d1599b323
Ludger Solbach <soulman@soulspace.org>
2025-06-07 23:47:51 +0200

    updated clay config

--- dev/dev.clj
+++ dev/dev.clj
@@ -1,6 +1,11 @@
 (ns dev
   (:require [scicloj.clay.v2.api :as clay]))
 
-(clay/make! {:source-path ["notebook/tutorial.clj"]
+(clay/make! {:base-source-path "notebook"
+             :source-path ["tutorial.clj"]
              :format [:html]
+             :title "QCLojure Docs"
+             :hide-ui-header true
+             :hide-info-line true
+             ;:live-reload true
              :browse true})

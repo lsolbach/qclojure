27bf5837631ed8a2726edcedaacf5f83d5947c85
Ludger Solbach <soulman@soulspace.org>
2025-06-11 11:51:16 +0200

    moved tests for the shor algorithm

--- test/org/soulspace/qclojure/application/algorithm/shors_algorithm_test.clj
+++ test/org/soulspace/qclojure/application/algorithm/shors_algorithm_test.clj
@@ -1,15 +1,16 @@
-(ns org.soulspace.qclojure.application.shors-algorithm-test
+(ns org.soulspace.qclojure.application.algorithm.shors-algorithm-test
   "Tests for Shor's factoring algorithm implementation"
   (:require [clojure.test :refer [deftest is testing run-tests]]
-            [org.soulspace.qclojure.application.algorithms :as qa]
-            [org.soulspace.qclojure.domain.math :as qmath]))
+            [org.soulspace.qclojure.domain.math :as qmath]
+            [org.soulspace.qclojure.application.algorithm.shor :as shor]
+            [org.soulspace.qclojure.adapter.backend.simulator :as sim]))
 
 ;; Test Shor's Algorithm with small composite numbers
-#_(deftest test-shors-algorithm
+(deftest test-shors-algorithm
   (testing "Factoring N=15"
     (let [N 15
           expected-factors #{3 5}
-          result (qa/shor-algorithm N)]
+          result (shor/shor-algorithm (sim/create-simulator) N)]
       
       (is (:success result) "Factorization should succeed")
       (is (= 2 (count (:factors result))) "Should find 2 factors")
@@ -18,8 +19,8 @@
   (testing "Factoring N=21"
     (let [N 21
           expected-factors #{3 7}
-          result (qa/shor-algorithm N)]
-      
+          result (shor/shor-algorithm (sim/create-simulator) N)]
+
       (is (:success result) "Factorization should succeed")
       (is (= 2 (count (:factors result))) "Should find 2 factors")
       (is (= expected-factors (set (:factors result))) "Should find factors 3 and 7")))
@@ -27,8 +28,8 @@
   (testing "Factoring even number N=14"
     (let [N 14
           expected-factors #{2 7}
-          result (qa/shor-algorithm N)]
-      
+          result (shor/shor-algorithm (sim/create-simulator) N)]
+
       (is (:success result) "Factorization should succeed")
       (is (= 2 (count (:factors result))) "Should find 2 factors")
       (is (= expected-factors (set (:factors result))) "Should find factors 2 and 7")
@@ -36,8 +37,8 @@
   
   (testing "Perfect power factorization N=27"
     (let [N 27  ; 3^3
-          result (qa/shor-algorithm N)]
-      
+          result (shor/shor-algorithm (sim/create-simulator) N)]
+
       (is (:success result) "Factorization should succeed")
       (is (= [3 3 3] (:factors result)) "Should find factors [3 3 3]"))))
 

0ad6c44617d703864973dd15e2cd68c46b44422e
Ludger Solbach <soulman@soulspace.org>
2025-08-04 10:32:13 +0200

    added linear-algebra namespace

---
+++ src/org/soulspace/qclojure/domain/linear_algebra.clj
@@ -0,0 +1,1 @@
+(ns org.soulspace.qclojure.domain.linear-algebra)
---
+++ test/org/soulspace/qclojure/domain/linear_algebra_test.clj
@@ -0,0 +1,5 @@
+(ns org.soulspace.qclojure.domain.linear-algebra-test
+  (:require 
+    [clojure.test :refer [deftest is testing run-tests]]
+    [org.soulspace.qclojure.domain.linear-algebra :as la]))
+

8660eb588a0479755f0e1fc6487b24f9da82c6e2
Ludger Solbach <soulman@soulspace.org>
2025-08-09 13:42:13 +0200

    added approx-matrix= functions

--- src/org/soulspace/qclojure/util/test.clj
+++ src/org/soulspace/qclojure/util/test.clj
@@ -9,3 +9,21 @@
   ([a b tolerance]
    (< (m/abs (- a b)) tolerance)))
 
+(defn approx-matrix=
+  "Check if two matrices (vec-of-vecs) are approximately equal within tolerance."
+  ([A B] (approx-matrix= A B 1e-10))
+  ([A B tolerance]
+   (and (= (count A) (count B))
+        (every? true?
+                (map (fn [ar br]
+                       (and (= (count ar) (count br))
+                            (every? true? (map (fn [x y] (approx= x y tolerance)) ar br))))
+                     A B)))))
+
+(defn approx-complex-matrix=
+  "Check if two complex matrices in SoA form {:real A :imag B} are approximately equal."
+  ([A B] (approx-complex-matrix= A B 1e-10))
+  ([A B tolerance]
+   (and (approx-matrix= (:real A) (:real B) tolerance)
+        (approx-matrix= (:imag A) (:imag B) tolerance))))
+

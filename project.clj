51f6875c7d2580eefb90d14aa4e4c1969772251d
Ludger Solbach <soulman@soulspace.org>
2025-08-12 00:48:51 +0200

    added initial neanderthal backend

--- project.clj
+++ project.clj
@@ -6,18 +6,20 @@
                  [org.clojure/data.json "2.5.1"]
                  [org.clojure/spec.alpha "0.5.238"]
                  [org.clojure/test.check "1.1.1"]
-                 [uncomplicate/neanderthal "0.55.0"] ; AOT compiled one stop with fast startup
-                 ;[org.uncomplicate/neanderthal-base "0.55.1"] ; Base library for Neanderthal, non AOT compiled
-                 ;[org.uncomplicate/neanderthal-opencl "0.55.0"] ; OpenCL backend for Neanderthal, non AOT compiled
-                 ;[org.uncomplicate/neanderthal-mkl "0.55.0"] ; MKL backend for Neanderthal, non AOT compiled
+                 ;[uncomplicate/neanderthal "0.55.0"] ; AOT compiled one stop with fast startup
+                 [org.uncomplicate/neanderthal-base "0.55.1"] ; Base library for Neanderthal, non AOT compiled
+                 [org.uncomplicate/neanderthal-opencl "0.55.0"] ; OpenCL backend for Neanderthal, non AOT compiled
+                 [org.uncomplicate/neanderthal-mkl "0.55.0"] ; MKL backend for Neanderthal, non AOT compiled
                  ;[org.uncomplicate/neanderthal-cuda "0.55.0"] ; CUDA backend for Neanderthal, non AOT compiled
                  ;[org.uncomplicate/neanderthal-accelerate "0.55.0"] ; Apple Accelerate backend for Neanderthal, non AOT compiled
                  [generateme/fastmath "3.0.0-alpha3"]
                  [hiccup/hiccup "2.0.0"]]
 
+  ; TODO: Provide native library path per platform
+  :jvm-opts ["-Djava.library.path=/usr/lib/x86_64-linux-gnu"]
   :profiles {:clay {:dependencies [[org.scicloj/clay "2-beta45"]]
                     :source-paths ["src" "notebook"]}}
-  
+
   :scm {:name "git" :url "https://github.com/lsolbach/qclojure"}
   :deploy-repositories [["clojars" {:sign-releases false :url "https://clojars.org/repo"}]]
   ;

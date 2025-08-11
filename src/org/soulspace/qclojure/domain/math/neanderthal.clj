bdbb07cf432a55d96003b4d4995d0ef38eca5814
Ludger Solbach <soulman@soulspace.org>
2025-08-11 11:14:15 +0200

    added initial neanderthal namespaces

---
+++ src/org/soulspace/qclojure/domain/math/neanderthal.clj
@@ -0,0 +1,1 @@
+(ns org.soulspace.qclojure.domain.math.neanderthal)
---
+++ test/org/soulspace/qclojure/domain/math/neanderthal_test.clj
@@ -0,0 +1,4 @@
+(ns org.soulspace.qclojure.domain.math.neanderthal-test
+  (:require [clojure.test :refer :all]
+            [org.soulspace.qclojure.domain.math.neanderthal :as m]
+            [org.soulspace.qclojure.test-utils :as util]))

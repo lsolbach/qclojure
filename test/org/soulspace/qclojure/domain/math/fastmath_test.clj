e6312de87e2ad6b6406d7c6f48d0c7536bbd2087
Ludger Solbach <soulman@soulspace.org>
2025-08-18 08:53:27 +0200

    added initial fasmath test namespace

---
+++ test/org/soulspace/qclojure/domain/math/fastmath_test.clj
@@ -0,0 +1,8 @@
+(ns org.soulspace.qclojure.domain.math.fastmath-test
+  (:require [clojure.test :refer [deftest is testing run-tests]]
+            [org.soulspace.qclojure.domain.math.fastmath :as fastmath]
+            [fastmath.core :as fm]
+            [fastmath.complex :as fc]
+            [fastmath.vector :as fvec]
+            [fastmath.matrix :as fmat]))
+

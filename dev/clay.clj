5115d94cf01745ace59998eee20a7c4ec6b9d41a
Ludger Solbach <soulman@soulspace.org>
2025-08-31 14:47:04 +0200

    improved tutorial generation

--- dev/clay.clj
+++ dev/clay.clj
@@ -2,18 +2,28 @@
   "Tools for building the QClojure tutorial notebook."
   (:require [scicloj.clay.v2.api :as clay]))
 
+(def tutorial-config
+  {:base-source-path "notebook"
+   :source-path ["tutorial.clj"]
+   :remote-repo {:git-url "https://github.com/lsolbach/qclojure"
+                 :branch "main"}
+   :format [:html]
+   :title "QClojure Tutorial"
+   :hide-ui-header true
+   :hide-info-line false
+   ;:live-reload true
+   :browse true})
+
+(defn make-tutorial-notebook
+  "Render the QClojure tutorial notebook to HTML."
+  ([]
+   (make-tutorial-notebook tutorial-config))
+  ([config]
+   (println "")
+   (println "Rendering the tutorial...")
+   (clay/make! config)))
+
 (comment ; Clay 
-  (do
-    (println "making clay")
-    (clay/make! {:base-source-path "notebook"
-                 :source-path ["tutorial.clj"]
-                 :remote-repo {:git-url "https://github.com/lsolbach/qclojure"
-                               :branch "main"}
-                 :format [:html]
-                 :title "QClojure Tutorial"
-                 :hide-ui-header true
-                 :hide-info-line false
-                 ;:live-reload true
-                 :browse true}))
+  (make-tutorial-notebook)
   ;
   )

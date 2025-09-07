3dd066e3c540f06393e0a14fd4d11b15c7531b1e
Ludger Solbach <soulman@soulspace.org>
2025-09-07 21:03:42 +0200

    added a load-resource function to io

--- src/org/soulspace/qclojure/util/io.clj
+++ src/org/soulspace/qclojure/util/io.clj
@@ -1,4 +1,6 @@
-(ns org.soulspace.qclojure.util.io)
+(ns org.soulspace.qclojure.util.io
+  "Utility functions for file and resource I/O operations."
+  (:require [clojure.java.io :as io]))
 
 ;; File I/O functions
 (defn save-file
@@ -14,3 +16,14 @@
   (spit filename content)
   filename)
 
+(defn load-resource
+  "Load a resource file from the classpath.
+   
+   Parameters:
+   -path: Resource path relative to classpath root
+   
+   Returns:
+   Sequence of lines from the resource"
+  [path]
+  (with-open [reader (io/reader (io/resource path))]
+    (doall (line-seq reader))))

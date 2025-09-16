0d28e341182ea594bc3c48d5640969285c8df3d0
Ludger Solbach <soulman@soulspace.org>
2025-09-16 02:31:44 +0200

    added initial namespaces for further algorithm templates

---
+++ src/org/soulspace/qclojure/application/algorithm/amplitude_estimation.clj
@@ -0,0 +1,4 @@
+(ns org.soulspace.qclojure.application.algorithm.amplitude-estimation
+  "Common infrastructure for sampling/amplitude estimation algorithms like QAE or IQAE."
+  (:require [org.soulspace.qclojure.domain.circuit :as circuit]
+            ))

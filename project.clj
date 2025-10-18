dc525b22a762064b3fcb1d135eb00e36b07bf0bf
Ludger Solbach <soulman@soulspace.org>
2025-10-18 12:14:44 +0200

    update Clay

--- project.clj
+++ project.clj
@@ -17,7 +17,7 @@
                                     "-XX:+AlwaysPreTouch"]}
              :container {:jvm-opts ["-XX:InitialRAMPercentage=2.0"
                                     "-XX:MaxRAMPercentage=60.0"]}
-             :clay {:dependencies [[org.scicloj/clay "2-beta57"]]
+             :clay {:dependencies [[org.scicloj/clay "2.0.0"]]
                     :source-paths ["src" "notebooks"]}}
 
   :scm {:name "git" :url "https://github.com/lsolbach/qclojure"}

4222ccf38bb8c4c3cf2ad6a30b360b531c77ea47
Ludger Solbach <soulman@soulspace.org>
2025-10-18 10:55:25 +0200

    update presentations

--- notebooks/qclojure_ai.clj
+++ notebooks/qclojure_ai.clj
@@ -8,58 +8,66 @@
 ;; * Clojure enthusiast since 2011
 ;; * [My GitHub page](https://github.com/lsolbach)
 ;; 
-;; # What is QClojure
-;;
-;; * A Clojure library for quantum computing
-;; * Quantum circuits and algorithms
-;; * Local simulators and cloud backends
-;; * Visualizations and analysis tools
-;; 
-;; # Motivation
+;; # The Motivation
 ;;
 ;; * Get experience with AI assisted programming
 ;; * Learn about quantum computing
 ;;
-;; # Project Setup
+;; # The Goal
+;;
+;; * A production ready library for quantum computing
+;;   * Quantum states, gates and circuits
+;;   * Library of quantum and hybrid algorithms
+;;   * Local simulators and cloud backends
+;;   * Visualizations and analysis tools
+;; 
+;; # Setup
 ;; ## IDE
 ;;
 ;; * VS Code - Editor
 ;; * Calva - Clojure IDE for VSCode
-;; * GitHub Copilot Pro - multiple AI models
 ;; * Calva Backseat Driver - MCP server with Clojure Tools
 ;; * Joyride
 ;;
-;; ## Models
+;; ## AI Models
 ;;
-;; * Claude Sonnet 4 - goto model for coding and documentation
-;; * GPT - good for research and planning
-;; * Gemini 2.5 Pro - prompt assessment
-;;
-;; ## Prompts
-;; * Chat Modes - setting the mode of interaction
-;; * Instructions - file type specific instructions
-;; * Prompts - task specific instructions
-;; * AGENTS.md - project specific instructions
+;; * GitHub Copilot Pro
+;;   * Claude Sonnet 4 - goto model for coding and documentation
+;;   * GPT - good for research and planning
+;;   * Gemini 2.5 Pro - prompt assessment
 ;;
 ;; ## MCP Tools
+;;
 ;; * REPL
 ;;   * testing and exploration
 ;; * human intelligence 
 ;;   * design discussions, planning, reviews
 ;;
+;; ## Prompts
+;;
+;; * AGENTS.md - project specific instructions
+;; * Instructions - file type specific instructions
+;; * Prompts - task specific instructions
+;; * Chat Modes - setting the mode of interaction
+;;
 ;; ## General Workflow
-;; 1. Ask mode (or ChatGPT) mode for research and planning
-;; 2. Agent mode for implementation, testing and docs
+;;
+;; 1. Use Ask mode (or ChatGPT) for research and planning
+;; 2. Use Agent mode for implementation, testing and docs
 ;; 
+;; # Demo
+;;
 ;; # Lessons Learned
 ;;
 ;; ## What Worked Well
-;; * bringing in domain knowledge
+;;
+;; * AI providing domain knowledge
 ;; * using the REPL for testing and exploration
 ;; * generating initial implementations
 ;; * generating documentation and comments
 ;;
 ;; ## Problems and Challenges
+;;
 ;; * bracket balancing
 ;;   * I'm much faster at fixing these issues
 ;; * need to refactor
@@ -68,20 +76,36 @@
 ;; * AI often builds simplified solutions
 ;;
 ;; ## Problems and Challenges
+;;
 ;; * context window is limited
 ;;   * dreaded "summarizing conversation history"
 ;;   * got better over time
-;; * the model does not learn
+;; * the AI does not learn
 ;; * quality of responses is inconsistent
 ;; * following instructions is inconsistent
-;;   * using the command line instead of the REPL
+;;   * e.g. using the command line instead of the REPL
 ;;
-;; # Summary
+;; ## Tips and Tricks
+;;
+;; * use different models for different tasks
+;; * use different models to assess your prompts
+;; * keep prompts short and precise
+;; * use the human intelligence to keep you in the loop
+;;  
+;; # Conclusions
+;;
+;; * AI is a good pair programmer, bringing in domain knowledge
+;; * AI is a bad learner, it does not learn from its mistakes
+;; * AI is not a replacement for human intelligence
+;;   * need to review, refactor and test everything
+;;
+;; # Final Thoughts
+;;
 ;; * Without AI assistence, I would not have started this project
 ;; * I've learned a lot about quantum computing and AI assisted programming
 ;; * Clojure is the perfect language for AI assisted programming
 ;;   * iterative REPL driven development with fast feedback for the AI
-;; * AI is a good pair programmer, bringing in domain knowledge
-;; * AI is a bad learner, it does not learn from its mistakes
-;; * AI is not a replacement for human intelligence
-;;   * need to review and test everything
+;;
+;; # Thank You!
+;; * Questions?
+;;

d49c7872a54d75d65ae9729efe9e3cdfb58e73f8
Ludger Solbach <soulman@soulspace.org>
2025-10-25 11:30:18 +0200

    add write-edn function

--- src/org/soulspace/qclojure/util/io.clj
+++ src/org/soulspace/qclojure/util/io.clj
@@ -2,6 +2,29 @@
   "Utility functions for file and resource I/O operations."
   (:require [clojure.java.io :as io]))
 
+(defn write-edn
+  "Write a Clojure object as EDN to a writer.
+   
+   Parameters:
+    - w: Writer to write to
+    - obj: Clojure object to serialize
+   
+   Returns: nil
+   
+   See https://ask.clojure.org/index.php/14203/how-to-persist-clojure-data-structures-to-disk?show=14205#a14205."
+  [w obj]
+  (binding [*print-length* nil
+            *print-level* nil
+            *print-dup* false
+            *print-meta* false
+            *print-readably* true
+
+            ;; namespaced maps not part of edn spec
+            *print-namespace-maps* false
+
+            *out* w]
+    (pr obj)))
+
 ;; File I/O functions
 (defn save-file
   "Save content to a file.
